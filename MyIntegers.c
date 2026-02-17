78446747d6f081c65406f94beabacd87f9819562
bpaskaran <brendan.paskaran@stonybrook.edu>
2026-02-17 18:55:39 +0000

    changes

--- MyIntegers.c
+++ MyIntegers.c
@@ -14,25 +14,24 @@ void repr_convert(char source_repr, char target_repr, unsigned int repr) {
       return;
    }
 
-   /* 2) Decode (turn the input bits into a signed number in 'value') */
    long long value = 0;
 
    if (source_repr == '2') {
-      /* two's complement: check sign bit (bit 31) */
+     
       if ((repr & 0x80000000U) != 0U) {
-         /* negative: value = repr - 2^32 */
+      
          value = (long long)repr;
          value = value - (1LL << 32);
       } else {
-         /* positive */
+       
          value = (long long)repr;
       }
    } else {
-      /* sign/magnitude: top bit is sign, rest is magnitude */
+     
       unsigned int sign = (repr >> 31) & 1U;
       unsigned int mag  = repr & 0x7FFFFFFFU;
 
-      /* treat both +0 and -0 as 0 */
+    
       if (mag == 0U) {
          value = 0;
       } else {
@@ -44,10 +43,10 @@ void repr_convert(char source_repr, char target_repr, unsigned int repr) {
       }
    }
 
-   /* 3) Convert to target and 4) print */
+
 
    if (target_repr == '2') {
-      /* must fit in signed 32-bit */
+    
       long long min = -(1LL << 31);
       long long max = (1LL << 31) - 1;
 
@@ -56,14 +55,14 @@ void repr_convert(char source_repr, char target_repr, unsigned int repr) {
          return;
       }
 
-      /* printing as 8 hex digits */
+    
       printf("%08x\n", (unsigned int)value);
       return;
    }
 
-   /* target is 'S' (sign/magnitude) */
+
    {
-      /* sign/magnitude cannot represent -2^31 */
+    
       long long min = -((1LL << 31) - 1);
       long long max =  ((1LL << 31) - 1);
 
@@ -77,7 +76,7 @@ void repr_convert(char source_repr, char target_repr, unsigned int repr) {
          return;
       }
 
-      /* build sign/magnitude bits */
+    
       unsigned int sign_bit = 0U;
       unsigned int mag = 0U;
 
@@ -89,7 +88,6 @@ void repr_convert(char source_repr, char target_repr, unsigned int repr) {
          mag = (unsigned int)value;
       }
 
-      /* put sign in bit 31, magnitude in bits 30..0 */
       unsigned int out = 0U;
       out = sign_bit << 31;
       out = out | (mag & 0x7FFFFFFFU);
--- Myfloat.c
+++ Myfloat.c
@@ -4,12 +4,24 @@
 float construct_float_sf(char sign_bit, char exponent, unsigned int fraction) {
    unsigned int f = 0;
 
+   unsigned int sign_part;
+   unsigned int exponent_part;
+   unsigned int fraction_part;
 
-   unsigned int sign = ((unsigned int)sign_bit & 1U) << 31;   
-   unsigned int exp  = ((unsigned int)exponent & 0xFFU) << 23; 
-   unsigned int frac = fraction & 0x7FFFFFU;                   
+   sign_part = (unsigned int)sign_bit;
+   sign_part = sign_part & 1U;      
+   sign_part = sign_part << 31;
 
-   f = sign | exp | frac;
+
+   exponent_part = (unsigned int)exponent;
+   exponent_part = exponent_part & 0xFFU;  
+   exponent_part = exponent_part << 23;
+
+   fraction_part = fraction & 0x7FFFFFU;
+
+   f = sign_part;
+   f = f | exponent_part;
+   f = f | fraction_part;
 
    return *(float *)&f;
 }
